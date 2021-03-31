6a53cd71fc8a8be7d13c07a7b3c6901039f73b31
qubra <qubra@DESKTOP-VEOETUV>
2021-04-01 02:54:08 +0300

    Kodlama.io

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -55,10 +55,10 @@ namespace Business.Concrete
         {
             // İş kodları yazılır
             // Yetkisi var mı..
-            if (DateTime.Now.Hour == 1)
-            {
-                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
-            }
+            //if (DateTime.Now.Hour == 1)
+            //{
+            //    return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+            //}
 
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductListed);
         }
